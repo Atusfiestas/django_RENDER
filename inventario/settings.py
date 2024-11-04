6656de9f25c2352429a3cb21ef45cb4db5a74e6c
Wilson Perez <168066896+Atusfiestas@users.noreply.github.com>
2024-11-04 12:38:31 -0500

    Update settings.py

--- inventario/settings.py
+++ inventario/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
